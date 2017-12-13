53589c046e243edfe16d1782eb3876820a4a91f8
Ragnarsmoke <emil.bertilsson@outlook.com>
2017-12-13 12:32:57 +0100

    Added run script

---
+++ console-calculator.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+jarfile="./ConsoleCalculators.jar"
+
+if [[ -s "$jarfile" ]]; then
+    java -jar $jarfile
+else
+    echo "Error! $jarfile not found! Make sure you have compiled the project with 'ant jar' first!"
+fi
